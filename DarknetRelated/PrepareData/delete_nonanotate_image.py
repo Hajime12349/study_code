2dd42b86896769a815a716e21428bee258b87c19
Hajime <qqwwkami@gmail.com>
2023-04-03 10:04:48 +0900

    a

--- DarknetRelated/PrepareData/delete_nonanotate_image.py
+++ DarknetRelated/PrepareData/delete_nonanotate_image.py
@@ -7,17 +7,17 @@ import os
 
 
 i=1
-while i<=500:
+while i<=2000:
     print(str(i))
     name_IMG='IMG_'+str(i)+'.jpg'
     name_TXT='IMG_'+str(i)+'.txt'
-
-    non_data=False
-    if not os.path.exists(name_TXT):
-        non_data=True
-    if(non_data):
-        mod_IMG='mod_'+name_IMG
-        os.rename(name_IMG,mod_IMG)
-        print('change:'+mod_IMG)
+    if(os.path.exists(name_IMG)):
+        non_data=False
+        if not os.path.exists(name_TXT):
+            non_data=True
+        if(non_data):
+            mod_IMG='mod_'+name_IMG
+            os.rename(name_IMG,mod_IMG)
+            print('change:'+mod_IMG)
     i+=1
 
