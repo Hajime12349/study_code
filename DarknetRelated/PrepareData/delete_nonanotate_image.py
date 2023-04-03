de48a0ef39dfef2aae4267c9236c7230e672ef64
Hajime <qqwwkami@gmail.com>
2023-04-03 09:58:40 +0900

    add delete_nonanottate_image

---
+++ DarknetRelated/PrepareData/delete_nonanotate_image.py
@@ -0,0 +1,23 @@
+#coding: utf-8
+
+import cv2
+import numpy as np
+from PIL import Image
+import os
+
+
+i=1
+while i<=500:
+    print(str(i))
+    name_IMG='IMG_'+str(i)+'.jpg'
+    name_TXT='IMG_'+str(i)+'.txt'
+
+    non_data=False
+    if not os.path.exists(name_TXT):
+        non_data=True
+    if(non_data):
+        mod_IMG='mod_'+name_IMG
+        os.rename(name_IMG,mod_IMG)
+        print('change:'+mod_IMG)
+    i+=1
+
