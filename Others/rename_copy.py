4659ac3b0d9c82bff7cb9e270eec124a9988ea03
Hajime12349 <qqwwkami@gmail.com>
2023-09-12 17:00:14 +0900

    change1 rename_copy

--- Others/rename_copy.py
+++ Others/rename_copy.py
@@ -1,8 +1,19 @@
 import shutil
 import pathlib
 
+start=4940
+end=9881
 input_dir=Path('/home/owner/taguchi/data/kikurage_data')
-output_dir=Path()
+output_dir=Path('/home/owner/taguchi/RetinaGan/cyclegan/datasets/obj/testA')
+count=0
+
 for num in range(start,end):
-    img_name=f'IMG_{num}.jpg'
-    txt_name=f'IMG_{num}.txt'
+    count=count+1
+    img_input=Path(f'IMG_{num}.jpg')
+    txt_input=Path(f'IMG_{num}.txt')
+    img_output=Path(f'IMG_{count}.jpg')
+    txt_output=Path(f'IMG_{count}.txt')
+    shutil.copyfile(input_dir/img_name,output_dir/img_output)
+    shutil.copyfile(input_dir/txt_name,output_dir/txt_output)
+
+
