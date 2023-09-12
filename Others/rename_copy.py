2b94de48c5c338a06767837fe0f545d89b0618eb
Hajime12349 <qqwwkami@gmail.com>
2023-09-12 16:56:13 +0900

    add rename_copy

---
+++ Others/rename_copy.py
@@ -0,0 +1,8 @@
+import shutil
+import pathlib
+
+input_dir=Path('/home/owner/taguchi/data/kikurage_data')
+output_dir=Path()
+for num in range(start,end):
+    img_name=f'IMG_{num}.jpg'
+    txt_name=f'IMG_{num}.txt'
